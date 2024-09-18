4752dd7f941e752ad3e95fc0a99144f4b8dda520
David Alberto Montaño Fetecua <david.montano@circleci.com>
2024-09-17 20:04:45 -0500

    fix: using double quotes on every env var used in auth.sh script

--- src/scripts/auth.sh
+++ src/scripts/auth.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-sf config set org-instance-url=${INSTANCE_URL} --global
+sf config set org-instance-url="${INSTANCE_URL}" --global
 sf auth jwt grant \
-    --client-id ${CONSUMER_KEY} \
+    --client-id "${CONSUMER_KEY}" \
     --jwt-key-file ./server.key \
-    --username ${DEFAULT_USERNAME} \
-    --instance-url ${INSTANCE_URL} \
+    --username "${DEFAULT_USERNAME}" \
+    --instance-url "${INSTANCE_URL}" \
     --set-default-dev-hub \
-    --alias ${DEFAULT_DEVHUB_USERNAME}
-sf config set org-api-version=${API_VERSION}
+    --alias "${DEFAULT_DEVHUB_USERNAME}"
+sf config set org-api-version="${API_VERSION}"
