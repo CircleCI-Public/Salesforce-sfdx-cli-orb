b333c3e6000a83f448839dd36e9beadf003400f2
Alain O'Dea <alain.odea@sequencebio.com>
2025-08-21 16:31:07 -0230

    FIX #34: Bug: auth not working
    - Interpret CONSUMER_KEY as containing the name of an environment variable, not its value (use ! to get that env var name for interpolation)
    - Interpret DEFAULT_DEVHUB_USERNAME as potentially containing environment variable substitutions and compute those with envsubstr)
    - Allow API version to be blank without failing with an error (do not attempt to set org-api-version to blank)

--- src/scripts/auth.sh
+++ src/scripts/auth.sh
@@ -2,10 +2,14 @@
 
 sf config set org-instance-url="${INSTANCE_URL}" --global
 sf auth jwt grant \
-    --client-id "${CONSUMER_KEY}" \
+    --client-id "${!CONSUMER_KEY}" \
     --jwt-key-file ./server.key \
     --username "${DEFAULT_USERNAME}" \
     --instance-url "${INSTANCE_URL}" \
     --set-default-dev-hub \
-    --alias "${DEFAULT_DEVHUB_USERNAME}"
-sf config set org-api-version="${API_VERSION}"
+    --alias "$(echo "${DEFAULT_DEVHUB_USERNAME}" | envsubst)"
+if [ -z "${API_VERSION}" ]; then
+  echo 'No API version set: leaving as default'
+else
+  sf config set org-api-version="${API_VERSION}"
+fi
